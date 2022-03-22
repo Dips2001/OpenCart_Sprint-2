f6190e7dfeb69810dc287f28c0a9022d8ea6b34a
Keshav7101 <keshav.choudhary_cs18@gla.ac.in>
2022-03-22 21:04:17 +0530

    Added some changes

--- OpenCart/src/test/java/com/runner/OpenCartRunner.java
+++ OpenCart/src/test/java/com/runner/OpenCartRunner.java
@@ -27,8 +27,8 @@ import io.cucumber.testng.PickleWrapper;
 
 @CucumberOptions(
 features="Features",
-tags="@Home",
-glue = "com.Home.stepDefinition",
+tags="@SortingProducts",
+glue = "com.BrowseCart.stepDefinition",
 plugin={"html:target/cucumber-html-report", "json:target/cucumber.json","pretty:target/cucumber-pretty.txt","usage:target/cucumber-usage.json", "junit:target/cucumber-results.xml"},
 dryRun = false,
 monochrome = true
